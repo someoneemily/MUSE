43149a30d63b6b62eeda0db034c43d0b5da0ed41
Emily Chen <emilycwx@mit.edu>
2021-01-26 21:35:20 -0500

    Initial Commit

---
+++ MUSE/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MUSE
+//
+//  Created by Emily chen on 1/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MUSE/MUSEApp.swift
@@ -0,0 +1,17 @@
+//
+//  MUSEApp.swift
+//  MUSE
+//
+//  Created by Emily chen on 1/26/21.
+//
+
+import SwiftUI
+
+@main
+struct MUSEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
